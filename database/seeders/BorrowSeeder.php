06b803f3c3c127579db4871c7fe97d29441c003e
MugiJR <immugi@yahoo.com>
2022-04-19 04:15:25 +0200

    Updated the BorrowSeeder temporary fix

--- database/seeders/BorrowSeeder.php
+++ database/seeders/BorrowSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Book;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
@@ -17,9 +18,102 @@ class BorrowSeeder extends Seeder
      */
     public function run()
     {
+        // DB::table('borrows')->truncate();
+        // Borrow::factory()
+        //     ->count(10)
+        //     ->create();
         DB::table('borrows')->truncate();
-        Borrow::factory()
-            ->count(10)
-            ->create();
+        $book = Book::all();
+        Borrow::create([
+            'reader_id' => 1,
+            'book_id' => Book::find(1)->id,
+            'status' => 'PENDING',
+            'request_process_at' => null,
+            'request_managed_by' => null,
+            'deadline' => null,
+            'returned_at' => null,
+            'return_managed_by' => null,
+        ]);
+        Borrow::create([
+            'reader_id' => 1,
+            'book_id' => Book::find(2)->id,
+            'status' => 'PENDING',
+            'request_process_at' => null,
+            'request_managed_by' => null,
+            'deadline' => null,
+            'returned_at' => null,
+            'return_managed_by' => null,
+        ]);
+        Borrow::create([
+            'reader_id' => 1,
+            'book_id' => Book::find(3)->id,
+            'status' => 'ACCEPTED',
+            'request_process_at' => now(),
+            'request_managed_by' => 2,
+            'deadline' => date('Y-m-d', strtotime( now() . " +5 days")),
+            'returned_at' => null,
+            'return_managed_by' => null,
+        ]);
+        Borrow::create([
+            'reader_id' => 1,
+            'book_id' => Book::find(4)->id,
+            'status' => 'ACCEPTED',
+            'request_process_at' => now(),
+            'request_managed_by' => 2,
+            'deadline' => date('Y-m-d', strtotime( now() . " +10 days")),
+            'returned_at' => null,
+            'return_managed_by' => null,
+        ]);
+        Borrow::create([
+            'reader_id' => 1,
+            'book_id' => Book::find(5)->id,
+            'status' => 'ACCEPTED',
+            'request_process_at' => now(),
+            'request_managed_by' => 2,
+            'deadline' => date('Y-m-d', strtotime( now() . " -5 days")),
+            'returned_at' => null,
+            'return_managed_by' => null,
+        ]);
+        Borrow::create([
+            'reader_id' => 1,
+            'book_id' => Book::find(6)->id,
+            'status' => 'ACCEPTED',
+            'request_process_at' => now(),
+            'request_managed_by' => 2,
+            'deadline' => date('Y-m-d', strtotime( now() . " -10 days")),
+            'returned_at' => null,
+            'return_managed_by' => null,
+        ]);
+        Borrow::create([
+            'reader_id' => 1,
+            'book_id' => Book::find(7)->id,
+            'status' => 'REJECTED',
+            'request_process_at' => now(),
+            'request_managed_by' => 2,
+            'deadline' => date('Y-m-d', strtotime( now() . " -10 days")),
+            'returned_at' => null,
+            'return_managed_by' => null,
+        ]);
+        Borrow::create([
+            'reader_id' => 1,
+            'book_id' => Book::find(8)->id,
+            'status' => 'REJECTED',
+            'request_process_at' => now(),
+            'request_managed_by' => 2,
+            'deadline' => date('Y-m-d', strtotime( now() . " +5 days")),
+            'returned_at' => null,
+            'return_managed_by' => null,
+        ]);
+        Borrow::create([
+            'reader_id' => 1,
+            'book_id' => Book::find(9)->id,
+            'status' => 'RETURNED',
+            'request_process_at' => now(),
+            'request_managed_by' => 2,
+            'deadline' => date('Y-m-d', strtotime( now() . " +5 days")),
+            'returned_at' => date('Y-m-d', strtotime( now() . " +3 days")),
+            'return_managed_by' => 2,
+        ]);
+
     }
 }
